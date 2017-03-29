6f8e6708f0c33df6c30bf01a1cc7fffdec8b0291
Daniel Roberts <droberts@dimagi.com>
2017-03-29 17:22:08 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='quickcache',
+    version='0.0.1',
+    description='caching has never been easier',
+    author='Dimagi',
+    author_email='dev@dimagi.com',
+    url='https://github.com/dimagi/quickcache',
+    packages=['quickcache'],
+    test_suite='test_quickcache',
+    install_requires=[],
+)
