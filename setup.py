4cb1b6b69b1351aad4f13d3d46d78abab5c5b05e
Daniel Miller <dmiller@dimagi.com>
2017-11-17 15:41:21 -0500

    Python 3 tests and classifiers

--- setup.py
+++ setup.py
@@ -14,4 +14,12 @@ setup(
     install_requires=[
         'six==1.11.0',
     ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
