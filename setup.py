808e541a992adfdbcca0d958385075b02c65a4f1
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-03-15 13:55:16 -0400

    Update minor version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='quickcache',
-    version='0.1.0',
+    version='0.2.0',
     description='caching has never been easier',
     author='Dimagi',
     author_email='dev@dimagi.com',
